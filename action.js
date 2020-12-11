f99d343edacaebe58e9a7d3771ce1de21ce731eb
Nikhil Math <erulemath@gmail.com>
2020-12-10 19:00:47 -0700

    initial Push

---
+++ action.js
@@ -0,0 +1,33 @@
+let leftSide = '';
+let rightSide = '';
+let operator = '';
+
+handleClick = (num) => {
+	if (operator === '') {
+		leftSide += num;
+		document.getElementById('leftside').innerHTML = leftSide;
+	} else {
+		rightSide += num;
+		document.getElementById('rightside').innerHTML = rightSide;
+	}
+};
+
+chooseOperator = (incomingOperator) => {
+	operator = incomingOperator;
+	document.getElementById('operator').innerHTML = operator;
+};
+
+evaluateAnswer = () => {
+	let answer = '';
+	switch (operator) {
+		case '+':
+			leftSide + rightSide;
+			break;
+		case '-':
+			break;
+		case '*':
+			break;
+	}
+
+	document.getElementById('answer').innerHTML = answer;
+};
